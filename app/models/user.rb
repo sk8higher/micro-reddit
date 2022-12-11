8de8ae5c022b29970d4f96fe870f94e5d7a14cd3
Nelly Kim <sk8high3r@ya.ru>
2022-12-11 19:33:48 +0300

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+
+  validates :username, presence: true, length: { minimum: 4, maximum: 12 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6, maximum: 16 }
 end
